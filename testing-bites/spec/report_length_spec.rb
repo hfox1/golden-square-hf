0c469863a2fa91b4775fd26f876dff790ba88a77
Harry Fox <49591810+hfox1@users.noreply.github.com>
2022-07-19 12:38:12 +0100

    challenge perhaps done

--- testing-bites/spec/report_length_spec.rb
+++ testing-bites/spec/report_length_spec.rb
@@ -1,5 +1,24 @@
 require 'report_length.rb'
 
-RSpec.describe "report_length method"
+RSpec.describe "report_length method" do
+  it "reports empty string accurately" do
+    result = report_length("")
+    expect(result).to eq "This string was 0 characters long."
+  end
 
-"This string was #{length} characters long."
+  it "reports a normal word accurately" do
+    result = report_length("house")
+    expect(result).to eq "This string was 5 characters long."
+  end
+
+  it "reports a sentence accurately" do
+    result = report_length("the quick fox jumped over the lazy brown dog")
+    expect(result).to eq "This string was 44 characters long."
+  end
+
+end
+
+
+
+
+# "This string was #{length} characters long."
